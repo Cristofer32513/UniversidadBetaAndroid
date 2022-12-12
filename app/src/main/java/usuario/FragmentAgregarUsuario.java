eaef6ac5b4e77126098b1e2e5d393b9601b7397f
Cristofer32513 <cristofer32513@gmali.com>
2022-12-12 01:44:22 -0600

    Se agrego soporte de ingles para alta usuarios.

--- app/src/main/java/usuario/FragmentAgregarUsuario.java
+++ app/src/main/java/usuario/FragmentAgregarUsuario.java
@@ -1,6 +1,5 @@
 package usuario;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,25 +11,14 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.android.volley.Request;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.example.colectau_beta.LoginActivity;
-import com.example.colectau_beta.MenuActivity;
 import com.example.colectau_beta.R;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-
 import controlador.BaseVolleyFragment;
-import modelos.Usuario;
 
 public class FragmentAgregarUsuario extends BaseVolleyFragment {
 
@@ -55,30 +43,22 @@ public class FragmentAgregarUsuario extends BaseVolleyFragment {
 
             if(validarCampos()) {
                 String url = "http://colectaubeta.atwebpages.com/api_altas_usuarios.php";
-                StringRequest recuest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        System.out.println("---    -"+response);
-                        if(response.equals("{\"exito\":true,\"mensaje\":\"Insercion correcta\"}")) {
-                            Toast.makeText(getContext(), "Usuario Agregado", Toast.LENGTH_LONG).show();
-                            Fragment nuevoFragmento = new FragmentUsuarios();
-                            FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-                            transaction.replace(R.id.content_frame, nuevoFragmento);
-                            transaction.commit();
-                            Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setTitle("Usuarios");
-                        } else {
-                            mostrarError(getString(R.string.error_agregar_usuario));
-                        }
+                StringRequest recuest = new StringRequest(Request.Method.POST, url, response -> {
+                    System.out.println("---    -"+response);
+                    if(response.equals("{\"exito\":true,\"mensaje\":\"Insercion correcta\"}")) {
+                        Toast.makeText(getContext(), "Usuario Agregado", Toast.LENGTH_LONG).show();
+                        Fragment nuevoFragmento = new FragmentUsuarios();
+                        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                        transaction.replace(R.id.content_frame, nuevoFragmento);
+                        transaction.commit();
+                        Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setTitle("Usuarios");
+                    } else {
+                        mostrarError(getString(R.string.error_agregar_usuario));
                     }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        mostrarError(getString(R.string.falla_api));
-                    }
-                }) {
+                }, error -> mostrarError(getString(R.string.falla_api))) {
                     @Override
                     protected Map<String, String> getParams() {
-                        Map<String, String> parametros = new HashMap<String, String>();
+                        Map<String, String> parametros = new HashMap<>();
                         parametros.put("username", cajaNombre.getText().toString());
                         parametros.put("passaword", cajaPassword1.getText().toString());
                         parametros.put("email", cajaCorreo.getText().toString());
@@ -90,23 +70,23 @@ public class FragmentAgregarUsuario extends BaseVolleyFragment {
             }
         });
         btnCancelar.setOnClickListener(view1 -> new AlertDialog.Builder(requireContext())
-                .setIcon(android.R.drawable.ic_dialog_alert).setTitle("Precaucion")
-                .setMessage("¿Esta seguro de cancelar el registro del usuario?")
-                .setPositiveButton("Si", (dialogInterface, i) -> {
+                .setIcon(android.R.drawable.ic_dialog_alert).setTitle(getString(R.string.precaucion))
+                .setMessage(getString(R.string.confirmacion_cancelar))
+                .setPositiveButton(getString(R.string.si), (dialogInterface, i) -> {
                     Fragment nuevoFragmento = new FragmentUsuarios();
                     FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
                     transaction.replace(R.id.content_frame, nuevoFragmento);
                     transaction.commit();
                     Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setTitle("Usuarios");
                 })
-                .setNegativeButton("No", (dialogInterface, i) -> dialogInterface.cancel())
+                .setNegativeButton(getString(R.string.no), (dialogInterface, i) -> dialogInterface.cancel())
                 .show());
 
         return view;
     }
 
     public void mostrarError(String error) {
-        new AlertDialog.Builder(getContext())
+        new AlertDialog.Builder(requireContext())
                 .setIcon(android.R.drawable.ic_dialog_alert).setTitle(getString(R.string.precaucion))
                 .setMessage(error)
                 .setPositiveButton(getString(R.string.entendido), (dialogInterface, i) -> dialogInterface.cancel()).show()
@@ -124,36 +104,36 @@ public class FragmentAgregarUsuario extends BaseVolleyFragment {
         String regexCorreo = "^(([^<>()\\[\\]\\\\.,;:\\s@”]+(\\.[^<>()\\[\\]\\\\.,;:\\s@”]+)*)|(“.+”))@((\\[[0–9]{1,3}\\.[0–9]{1,3}\\.[0–9]{1,3}\\.[0–9]{1,3}])|(([a-zA-Z\\-0–9]+\\.)+[a-zA-Z]{2,}))$";
 
         if(cajaNombre.getText().toString().trim().isEmpty()) {
-            cadenaRespuesta.append("- Ingresa un nombre de usuario. \n\n");
+            cadenaRespuesta.append(getString(R.string.ingresa_nombre_usuario)).append("\n\n");
             cajaNombre.setBackgroundResource(R.drawable.borde_cajas_donativo_error);
             respuesta = false;
         }
 
         if(cajaCorreo.getText().toString().trim().isEmpty()) {
-            cadenaRespuesta.append("- Ingresa un email. \n\n");
+            cadenaRespuesta.append(getString(R.string.ingresa_email)).append("\n\n");
             cajaCorreo.setBackgroundResource(R.drawable.borde_cajas_donativo_error);
             respuesta = false;
         } else {
             if(!cajaCorreo.getText().toString().trim().matches(regexCorreo)) {
-                cadenaRespuesta.append("- Email no valido. \n\n");
+                cadenaRespuesta.append(getString(R.string.email_no_valido)).append("\n\n");
                 cajaCorreo.setBackgroundResource(R.drawable.borde_cajas_donativo_error);
                 respuesta = false;
             }
         }
 
         if(cajaPassword1.getText().toString().trim().isEmpty()) {
-            cadenaRespuesta.append("- Ingresa una contraseña. \n\n");
+            cadenaRespuesta.append(getString(R.string.ingresa_contraseña)).append("\n\n");
             cajaPassword1.setBackgroundResource(R.drawable.borde_cajas_donativo_error);
             respuesta = false;
         }
 
         if(cajaPassword2.getText().toString().trim().isEmpty()) {
-            cadenaRespuesta.append("- Confirma tu contraseña. \n\n");
+            cadenaRespuesta.append(getString(R.string.confirma_contraseña)).append("\n\n");
             cajaPassword2.setBackgroundResource(R.drawable.borde_cajas_donativo_error);
             respuesta = false;
         } else {
             if(!cajaPassword2.getText().toString().trim().equals(cajaPassword1.getText().toString().trim())) {
-                cadenaRespuesta.append("- Error en confirmacion de contraseña. \n\n");
+                cadenaRespuesta.append(getString(R.string.error_confirmacion_contraseña)).append("\n\n");
                 cajaPassword1.setBackgroundResource(R.drawable.borde_cajas_donativo_error);
                 cajaPassword2.setBackgroundResource(R.drawable.borde_cajas_donativo_error);
                 respuesta = false;
@@ -162,9 +142,9 @@ public class FragmentAgregarUsuario extends BaseVolleyFragment {
 
         if(!respuesta) {
             new AlertDialog.Builder(requireContext())
-                    .setIcon(android.R.drawable.ic_dialog_alert).setTitle("Errores detectados")
+                    .setIcon(android.R.drawable.ic_dialog_alert).setTitle(getString(R.string.errores_detectados))
                     .setMessage(cadenaRespuesta)
-                    .setPositiveButton("Entendido", (dialogInterface, i) -> dialogInterface.cancel())
+                    .setPositiveButton(getString(R.string.entendido), (dialogInterface, i) -> dialogInterface.cancel())
                     .show()
             ;
         }
